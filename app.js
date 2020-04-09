1b72fd0a669b4880da5477f6638899dc9ca57c1b
core97 <jcortesg1997@gmail.com>
2020-04-09 12:48:57 +0200

    Initialized project

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  return next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  const errorMessages = {
+    404: 'Page not found',
+  };
+  return res.status(err.status || 500).json(errorMessages[err.status] || err.message);
+});
+
+module.exports = app;
